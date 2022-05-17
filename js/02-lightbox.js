809511f24d92382f08008de0f8844797a1290957
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-05-17 13:20:17 +0200

    task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const result = galleryItems
+	.map(
+		({
+			preview,
+			original,
+			description,
+		}) => `<a class="gallery__item" href="${original}">
+	<img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+	)
+	.join("");
+
+gallery.insertAdjacentHTML("afterbegin", result);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+	captionPosition: "outside",
+	captionsData: "alt",
+	captionDelay: "250",
+});
