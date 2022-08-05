e3c6afde492b762dba50fef93218639549c2370d
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-08-05 13:48:41 +0200

    upgrade

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,38 +1,43 @@
+"use strict";
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
 
+// Change code below this line
 const gallery = document.querySelector(".gallery");
 
-const result = galleryItems
+const markup = galleryItems
 	.map(
-		({
-			preview,
-			original,
-			description,
-		}) => `<div class="gallery__item"><a class="gallery__link" href="${original}">
-		<img
-			class="gallery__image"
-			src="${preview}"
-			data-source="${original}"
-			alt="${description}"/>
-	</a></div>`
+		({ preview, original, description }) =>
+			`<div class="gallery__item">
+					<a class="gallery__link" href=${original}>
+						<img
+							class="gallery__image"
+							src=${preview}
+							data-source=${original}
+							alt=${description}
+						/>
+					</a>
+				</div>`
 	)
 	.join("");
 
-gallery.insertAdjacentHTML("afterbegin", result);
+gallery.insertAdjacentHTML("beforeend", markup);
 
-gallery.onclick = function newWindow(event) {
-	event.preventDefault();
-	if (event.target.nodeName !== "IMG") {
+const handlerGallery = (e) => {
+	e.preventDefault();
+	if (e.target.nodeName !== "IMG") {
 		return;
 	}
-	const modal = basicLightbox.create(
-		`<img src = ${event.target.dataset.source}>`
-	);
+	const modal = basicLightbox.create(`<img src=${e.target.dataset.source}>`);
+
 	modal.show();
-	window.addEventListener("keydown", function quit(event) {
-		if (event.key === "Escape") {
+
+	const exit = (e) => {
+		if (e.key === "Escape") {
 			modal.close();
+			window.removeEventListener("keydown", exit);
 		}
-	});
+	};
+	window.addEventListener("keydown", exit);
 };
+
+gallery.addEventListener("click", handlerGallery);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,24 +1,22 @@
+"use strict";
+
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
 
-const result = galleryItems
+const markup = galleryItems
 	.map(
-		({
-			preview,
-			original,
-			description,
-		}) => `<a class="gallery__item" href="${original}">
-	<img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>`
+		({ preview, original, description }) =>
+			`<a class="gallery__item" href=${original}>
+  <img class="gallery__image" src=${preview} alt=${description} />
+</a>`
 	)
 	.join("");
 
-gallery.insertAdjacentHTML("afterbegin", result);
+gallery.insertAdjacentHTML("beforeend", markup);
 
-const lightbox = new SimpleLightbox(".gallery a", {
-	captionPosition: "outside",
+let lightbox = new SimpleLightbox(".gallery a", {
 	captionsData: "alt",
-	captionDelay: "250",
+	captionDelay: 250,
 });
